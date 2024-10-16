7e27f6b746bd3690594cea3d31319071703e5896
chitoshimorishige <chitoshimorishige@gmail.com>
2024-10-17 08:34:04 +0900

    add main

--- show.py
+++ show.py
@@ -7,7 +7,7 @@ def main():
 
     with open("./hyakunin.txt", encoding="utf-8") as f:
         wakas = [s.strip() for s in f.readlines()]
-    print(wakas[random.randrange(len(wakas))])
+        print(wakas[random.randrange(len(wakas))])
 
-if __name__ == '__main__':
+if __name__ == '__main_git_':
     main()
